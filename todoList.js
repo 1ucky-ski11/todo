593f917ad3f2fe158b22d1c320ff6d9ccd30af8b
1ucky-ski11 <umarbek07210@gmail.com>
2024-09-16 23:03:20 +0500

    TodoList

---
+++ todoList.js
@@ -0,0 +1,70 @@
+// "use strict";
+//
+// const todoList = ['uxlash','ovqat yeyish']
+// renderTodo()
+// function renderTodo(){
+//     let todoListHTML = ''
+//
+//     for (let i = 0; i< todoList.length; i++){
+//         const todo = todoList[i]
+//         const html = <p>${todo}</p>
+//         todoListHTML += html
+//     }
+//     console.log(todoListHTML)
+//     document.querySelector('.todo-list').innerHTML = todoListHTML
+// }
+// function getTodo(){
+//     const inputElement = document.querySelector('.get-todo')
+//     const name = inputElement.value
+//     todoList.push(name)
+//     console.log(todoList)
+//     inputElement.value = ''
+//     renderTodo()
+// }
+
+
+
+
+//11 A
+
+// const num = [10,20,30]
+// num[2] = 99
+// console.log(num)
+
+
+// 11 B
+
+// function getLastValue(massive){
+//     console.log(massive[massive.length - 1])
+// }
+// getLastValue(['1','2','3',true,false])
+
+//11 C
+
+// function arraySwap(massive){
+//
+//     let temp = massive[0]
+//     massive[0] = massive[massive.length -1]
+//     massive[massive.length -1] = temp
+//     console.log(massive)
+// }
+// arraySwap([1,2,3,4,5])
+
+//11 D
+
+// for (let i = 0; i <= 10; i+=2) {
+//     console.log(i)
+// }
+//
+// for (let i = 1; i <= 10; i+=2) {
+//     console.log(i)
+// }
+
+//11 E
+
+// for (let i = 0; i <=5; i++) {
+//      console.log(i)
+// }
+
+//11 F
+// ......
